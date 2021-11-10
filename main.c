c41bd9930893a9e52119385504a5dad65a5b9354
jennisun <jsun20@stuy.edu>
2021-11-09 21:49:41 -0500

    wrote SIGUSR1

--- main.c
+++ main.c
@@ -11,19 +11,25 @@
 static void sighandler(int signo) {
   if (signo == SIGINT) {
     int file = open("signals.txt", O_CREAT | O_WRONLY | O_APPEND, 0644);
+    if (file < 0) {
+      printf("%s\n", strerror(errno));
+      return;
+    }
+
     char arr[50] = "Program quit due to SIGINT\n";
     write(file, arr, sizeof(arr));
     exit(0);
   }
-
+  if (signo == SIGUSR1) printf("Parent process PID: %d\n", getppid());
 }
 
 int main() {
   signal(SIGINT, sighandler);
+  signal(SIGUSR1, sighandler);
 
   int a = 1;
   while (a > 0) {
-    printf("PID %d\n", getpid());
+    printf("PID: %d\n", getpid());
     sleep(1);
   }
   return 0;
